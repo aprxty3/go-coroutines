11f102792efbff1ff3006bbf0a0ea87d294ab9c2
aji prasetyo <ajiprxty3@gmail.com>
2025-04-12 04:14:41 +0700

    update

--- goroutines_test.go
+++ goroutines_test.go
@@ -16,3 +16,15 @@ func TestCreateGoroutines(t *testing.T) {
 
 	time.Sleep(1 * time.Second)
 }
+
+func DisplayNumber(num int) {
+	fmt.Println("Dipslay Number:", num)
+}
+
+func TestManyGoRoutines(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(10 * time.Second)
+}
