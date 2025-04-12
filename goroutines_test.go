56dcc741b8aa67bd623ff9f9fd76d888afb7df56
aji prasetyo <ajiprxty3@gmail.com>
2025-04-12 21:53:16 +0700

    channel 2

--- goroutines_test.go
+++ goroutines_test.go
@@ -2,6 +2,7 @@ package go_coroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -77,4 +78,64 @@ func TestInOutChannel(t *testing.T) {
 
 	time.Sleep(2 * time.Second)
 	close(channel)
+}
+
+func TestBufferChannel(t *testing.T) {
+	channel := make(chan string, 4)
+	defer close(channel)
+
+	go func() {
+		channel <- "Hello World"
+		channel <- "Aji"
+		channel <- "Pras"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(1 * time.Second)
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Data from channel:", data)
+	}
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveResponse(channel1)
+	go GiveResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from channel 1:", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from channel 2:", data)
+			counter++
+		}
+		if counter == 2 {
+			fmt.Println("All data received")
+			return
+		}
+	}
 }
