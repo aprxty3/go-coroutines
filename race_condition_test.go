6ae478a68559a16bf7dc5b935e9d9a095952f33f
aji prasetyo <ajiprxty3@gmail.com>
2025-04-12 23:18:39 +0700

    deadlock

--- race_condition_test.go
+++ race_condition_test.go
@@ -74,3 +74,42 @@ func TestReadWriteMutext(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("count:", account.ReadBalance())
 }
+
+type UserBalance struct {
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBalance) Deposit(amount int) {
+	user.Balance = user.Balance + amount
+}
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock()
+	fmt.Println("Locking user1", user1.Name)
+	user1.Deposit(-amount)
+	time.Sleep(1 * time.Second)
+
+	user2.Lock()
+	fmt.Println("Locking user2", user2.Name)
+	user2.Deposit(amount)
+	time.Sleep(1 * time.Second)
+
+	user1.Unlock()
+	user2.Unlock()
+}
+
+func TestDeadlock(t *testing.T) {
+	user1 := &UserBalance{Name: "user1", Balance: 1000}
+	user2 := &UserBalance{Name: "user2", Balance: 1000}
+
+	go Transfer(user1, user2, 100)
+	go Transfer(user2, user1, 200)
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("user", user1.Name, "balance:", user1.Balance)
+	fmt.Println("user", user2.Name, "balance:", user2.Balance)
+
+}
